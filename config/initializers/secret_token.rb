ff5761c085f7dbd4cc6add0939abcc9817842e12
Oleg Sobyna <o.sobyna@gmail.com>
2013-07-14 12:07:26 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColoMS::Application.config.secret_token = '614feb152984fb6ee2620bf866f46d25678de488081b3551c6b90de3de2af517dfeafae8bda753361681f5f17ea81e9cf56e0067393c50ed0ff893b2ae814cf9'
