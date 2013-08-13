6c2b0c2a908ba7098faae1990ed7fc270672f85a
xinetd32 <xinetd@ukr.net>
2013-08-13 13:04:49 +0300

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 r = Role.create(name: 'admin', description: 'Administrator')
 r = Role.create(name: 'guest', description: 'Guest')
-u = User.new(email: 'xinetd@ukr.net', password: "123", first_name: 'Oleg', last_name: 'Sobyna')
+u = User.new(email: 'os@colobridge.net', password: "123", first_name: 'Oleg', last_name: 'Sobyna')
+u.add_role('admin')
+u.save
+
+u = User.new(email: 'km@colobridge.net', password: "12345678", first_name: 'Kirill', last_name: 'Marchenko')
+u.add_role('admin')
+u.save
+
+u = User.new(email: 'a.zhaurov@colobridge.net', password: "12345678", first_name: 'Alexey', last_name: 'Zhaurov')
 u.add_role('admin')
 u.save
 
@@ -17,14 +25,14 @@ u.save
 
 v=Vendor.create(name: 'Cisco', description: 'Cisco Inc.')
 p=ProductType.create(name: 'Router')
-m=Model.new(name: '2811/K9', description: 'Security bundle')
+m=Model.create(name: '2811/K9', description: 'Security bundle')
 m.vendor = v
 m.product_type = p
 m.save
 
 v=Vendor.create(name: 'HP', description: 'HP')
 p=ProductType.create(name: 'Switch')
-m=Model.new(name: 'ProCurve', description: '')
+m=Model.create(name: 'ProCurve 2650', description: '28 10/100 ports + 4 SFP')
 m.vendor = v
 m.product_type = p
 m.save
